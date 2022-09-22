d2d393b10bd317243f29492c0c0744cee5f1dc65
Ayush Kumar Pandey <badsoulak@gmail.com>
2022-09-23 00:37:14 +0530

    JS-101_Learning_Javascript/Introduction_to_Javascript/(All replit program files)

---
+++ problem1.js
@@ -0,0 +1,8 @@
+
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+
+let name="Masai School";
+let title="A Transformation in Education";
+console.log(name);
+console.log(title);
---
+++ problem4.js
@@ -0,0 +1,25 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+
+
+console.log( "*****************************************************"    );
+console.log( "*                  St. Paul's High School           *"    );
+console.log( "*                      2021-2022                    *"    );
+console.log( "*****************************************************"    );
+console.log( "*Name- Ayush   Class- Xth   Roll- 15   Sec- C       *"    );
+console.log( "*~~~~~~~~~||~~~~~~~~~~||~~~~~~~~~~||~~~~~~~||~~~~~~~*"    );
+console.log( "* Subject || Internal || External || Total || Grade *"    );
+console.log( "*~~~~~~~~~||~~~~~~~~~~||~~~~~~~~~~||~~~~~~~||~~~~~~~*"    );
+console.log( "* Science ||   46     ||   48     ||  94   ||   A   *"    );
+console.log( "*         ||          ||          ||       ||       *"    );
+console.log( "* English ||   45     ||   46     ||  91   ||   A   *"    );
+console.log( "*         ||          ||          ||       ||       *"    );
+console.log( "*  Maths  ||   49     ||   47     ||  96   ||   A   *"    );
+console.log( "*         ||          ||          ||       ||       *"    );
+console.log( "*****************************************************"    );
+console.log("                                          Grade ~ A"       );
+console.log("  _____________    __________________    ____________  "   );
+console.log(" Principal Sign    Class teacher sign    Parents Sign"     ); 
